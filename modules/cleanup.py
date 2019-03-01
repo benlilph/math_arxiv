8e9238e5820e9d73522e3a95d9d6aace39ee0163
benlilph <pl473@cornell.edu>
2019-02-28 22:41:36 -0500

    Add files via upload
    This is a module to cleanup the text

---
+++ modules/cleanup.py
@@ -0,0 +1,65 @@
+import re
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.stem import PorterStemmer
+
+# make a class such that we can use sklearn gridsearchCV and pipeline
+
+
+class cleanup(object):
+    def __init__(self):
+        pass
+
+    @classmethod
+    def cancelmath(cls, test_str):
+        regex = r"(\$+)(?:(?!\1)[\s\S])*\1"
+
+        subst = ""
+
+        result = re.sub(regex, subst, test_str, 0, re.MULTILINE)
+
+        return result
+
+    @classmethod
+    def delete_stopword_mathsymbols(cls, text):
+        temp = []
+        stop = stopwords.words('english')
+        for x in word_tokenize(text):
+            if x.lower() in stop or len(x) == 1 or x.isdigit() or ("\\" in x) or ("^" in x):
+                continue
+
+            temp += [x.lower()]
+        return temp
+
+    @classmethod
+    def delete_double_digit(cls, tokens):
+        # given a token , if it contains more than 2 numbers , then probably it is useless
+        empty = []
+        num_pat = re.compile(r'[0-9][0-9]')
+        for x in tokens:
+            if len(num_pat.findall(x)) == 0:
+                empty += [x]
+        return empty
+
+    @classmethod
+    def stem_words(cls, tokens):
+        stemmer = PorterStemmer()
+        pat = re.compile(r'[\\\!\@\#\$\%\^\&\*\(\)\_\=\+]')
+        empty = []
+        for x in tokens:
+            if len(pat.findall(x)) == 0:
+                empty += [stemmer.stem(x)]
+        # text=pat.sub("",text)
+        # tokensss=word_tokenize(text)
+        return " ".join(empty)
+
+    def fit(self, text1, text2):
+        return self
+
+    def transform(self, text):
+        text = cleanup.cancelmath(text)
+        tokens = cleanup.delete_stopword_mathsymbols(text)
+        tokens = cleanup.delete_double_digit(tokens)
+        text = cleanup.stem_words(tokens)
+        return text
