df94b5a06312e1ec889bdb1beee3a63b6ab5a55a
benlilph <pl473@cornell.edu>
2019-02-28 22:55:11 -0500

    Update cleanup.py

--- modules/cleanup.py
+++ modules/cleanup.py
@@ -6,21 +6,20 @@ from nltk.stem import PorterStemmer
 
 # make a class such that we can use sklearn gridsearchCV and pipeline
 
-
 class cleanup(object):
-    def __init__(self):
-        pass
-
+    def __init__(self,stem_on=False):
+        self.stem_on=stem_on
+    
     @classmethod
     def cancelmath(cls, test_str):
         regex = r"(\$+)(?:(?!\1)[\s\S])*\1"
-
+        
         subst = ""
-
+        
         result = re.sub(regex, subst, test_str, 0, re.MULTILINE)
-
+        
         return result
-
+    
     @classmethod
     def delete_stopword_mathsymbols(cls, text):
         temp = []
@@ -28,10 +27,10 @@ class cleanup(object):
         for x in word_tokenize(text):
             if x.lower() in stop or len(x) == 1 or x.isdigit() or ("\\" in x) or ("^" in x):
                 continue
-
+            
             temp += [x.lower()]
         return temp
-
+    
     @classmethod
     def delete_double_digit(cls, tokens):
         # given a token , if it contains more than 2 numbers , then probably it is useless
@@ -41,25 +40,38 @@ class cleanup(object):
             if len(num_pat.findall(x)) == 0:
                 empty += [x]
         return empty
-
+    
     @classmethod
     def stem_words(cls, tokens):
         stemmer = PorterStemmer()
-        pat = re.compile(r'[\\\!\@\#\$\%\^\&\*\(\)\_\=\+]')
+        #pat = re.compile(r'[\\\!\@\#\$\%\^\&\*\(\)\_\=\+]')
         empty = []
         for x in tokens:
-            if len(pat.findall(x)) == 0:
-                empty += [stemmer.stem(x)]
+            #if len(pat.findall(x)) == 0:
+            empty += [stemmer.stem(x)]
         # text=pat.sub("",text)
         # tokensss=word_tokenize(text)
-        return " ".join(empty)
-
+        return empty
+    
+    @classmethod
+    def delete_all_symbol(cls, tokens):
+        ###don't delete "-", we have something like 3-form
+        empty=[]
+        pat = re.compile(r'[\\\!\@\#\$\%\^\&\*\(\)\_\=\+]')
+        for x in tokens:
+            if len(pat.findall(x)) == 0:
+                empty += [x.lower()]
+        return empty
+    
     def fit(self, text1, text2):
         return self
-
+    
     def transform(self, text):
         text = cleanup.cancelmath(text)
         tokens = cleanup.delete_stopword_mathsymbols(text)
         tokens = cleanup.delete_double_digit(tokens)
-        text = cleanup.stem_words(tokens)
+        if self.stem_on:
+            tokens = cleanup.stem_words(tokens)
+        tokens = cleanup.delete_all_symbol(tokens)
+        text=" ".join(tokens)
         return text
